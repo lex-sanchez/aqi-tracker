273d3299866eafeec05e5d87960af4439c055a90
lex-sanchez <alexmsanchez2@gmail.com>
2020-10-07 12:50:14 -0700

    Updates

---
+++ precache-manifest.e30c91f1045276c2d056a7c620815bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aab88d6794217efbe162d0892b6afcd",
+    "url": "/aqi-tracker/index.html"
+  },
+  {
+    "revision": "81d031a9cc6a7aa89f19",
+    "url": "/aqi-tracker/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "156a5311231995de7f6e",
+    "url": "/aqi-tracker/static/css/main.d18f508a.chunk.css"
+  },
+  {
+    "revision": "81d031a9cc6a7aa89f19",
+    "url": "/aqi-tracker/static/js/2.e3772661.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aqi-tracker/static/js/2.e3772661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156a5311231995de7f6e",
+    "url": "/aqi-tracker/static/js/main.936925a7.chunk.js"
+  },
+  {
+    "revision": "e9e0dcb1daa98b8320b6",
+    "url": "/aqi-tracker/static/js/runtime-main.f563716d.js"
+  }
+]);
